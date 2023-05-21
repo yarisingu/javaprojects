f94fb663703b0d73764cad734925b0f876af1abd
yarisingu <yarisingutarun@gmail.com>
2023-05-21 22:23:40 +0530

    app ground

---
+++ Myfirstjava/src/Myfirstpackage/Demo.java
@@ -0,0 +1,16 @@
+package Myfirstpackage;
+
+public class Demo {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<5;j++)
+            {
+              System.out.print("my name is tarun    ");
+            }
+            System.out.println();
+        }
+    }
+
+    // print the String in matrix form with nested for loop
+}
